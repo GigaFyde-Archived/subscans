5f4b10eb4fbb090f70751cf65272ce77b85ba878
Laurant Marijnissen <laurant@gigafyde.dev>
2021-02-05 21:32:49 +0100

    Properly format the message

--- src/main/java/dev/gigafyde/subscans/listeners/RoleListener.java
+++ src/main/java/dev/gigafyde/subscans/listeners/RoleListener.java
@@ -10,7 +10,7 @@ public class RoleListener extends ListenerAdapter {
     @Override
     public void onGuildMemberRoleAdd(@NotNull GuildMemberRoleAddEvent event) {
         if (event.getRoles().contains(Main.jda.getRoleById("806161918028677130"))) {
-            Objects.requireNonNull(Main.jda.getTextChannelById("806163761097474118")).sendMessage("**Thank you so much for supporting us! " + event.getMember().getAsMention()).queue();
+            Objects.requireNonNull(Main.jda.getTextChannelById("806163761097474118")).sendMessage("**Thank you so much for supporting us!** " + event.getMember().getAsMention()).queue();
         }
     }
 }
